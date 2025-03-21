a517f666ce7e97128793fc6dc42b3eb1e7b9bf1f
Joel Bello <Joel8bl@gmail.com>
2025-03-21 01:34:12 +0100

    fix

--- dist/handlers/user.d.ts
+++
@@ -1,3 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-export declare const createNewUser: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-export declare const signin: (req: Request, res: Response) => Promise<void>;
